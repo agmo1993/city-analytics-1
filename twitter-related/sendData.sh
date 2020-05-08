4d207924d3b398dff2252882777df1f6257d46bc
agmo1993 <53358586+agmo1993@users.noreply.github.com>
2020-05-08 12:42:45 +1000

    shell script to couchdb added

--- twitter-related/sendData.sh
+++ twitter-related/sendData.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 while true
 do
-python txt_to_json_4.py
+python txt_to_json_2.py
 
 curl -XPOST "http://${user}:${pass}@${masternode}:5984/twitter/_bulk_docs " --header "Content-Type: application/json"   --data @ha$
 rm harvested.json

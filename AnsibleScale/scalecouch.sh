c9ac666f78021fcbf31e5d56c3ddc82f92e3942c
agmo1993 <53358586+agmo1993@users.noreply.github.com>
2020-05-25 21:41:15 +1000

    ansible fix

--- AnsibleScale/scalecouch.sh
+++ AnsibleScale/scalecouch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # set n to 1
 n=1
-now=$(date +'%d%m%Y')
+now=$(date +'%d%m%Y%H%M%S')
 
 # continue until $n equals 5
 while [ $n -le $1 ]
 do
 	echo "Welcome scaling cluster $n"
-    sed -i -e 's/addition_instance=[0-9].*/addition_instance='"${n}"''"${now}"'/g' ./inventory/hosts.ini
+    sed -i -e 's/addition_instance=[0-9].*/addition_instance='"${now}"'/g' ./inventory/hosts.ini
     ./launch-nectar-servers.sh
     ./install-environments.sh
 	n=$(( n+1 ))	 # increments $n

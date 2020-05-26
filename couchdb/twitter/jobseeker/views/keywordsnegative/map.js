89f2dba76e88221f6c20954ef273004b49abe74c
agmo1993 <53358586+agmo1993@users.noreply.github.com>
2020-05-26 13:16:24 +1000

    update views

--- couchdb/twitter/jobseeker/views/keywordsnegative/map.js
+++ couchdb/twitter/jobseeker/views/keywordsnegative/map.js
@@ -1,15 +1,11 @@
 function (doc) {
+  var climate = ['jobseeker','unemployment','centrelink','jobkeeper'];
   var words = doc.text.toLowerCase().split(/\W+/);
   var words2 = doc.extended_tweet.full_text.toLowerCase().split(/\W+/);
-  if (words.includes("jobseeker")|| words.includes("unemployment") || words.includes("centrelink")){
-    if (doc.sentiment < 0) { 
-      emit([doc.place.bounding_box.coordinates], 1);
-    }
+  if ((words.filter(value => climate.includes(value))).length > 0 && (doc.sentiment < 0)){
+      emit([doc.place.bounding_box.coordinates], doc.sentiment);
   }
-  else if (words2.includes("jobseeker")|| words2.includes("#unemployment") || words2.includes("centrelink")){
-    if (doc.sentiment < 0) { 
-    emit([doc.place.bounding_box.coordinates], 1);
-    }
+  else if ((words2.filter(value => climate.includes(value))).length > 0 && (doc.sentiment < 0)){
+    emit([doc.place.bounding_box.coordinates], doc.sentiment);
   }
-  
 }

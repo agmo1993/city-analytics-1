05d3cace23e660b4ec9fd70cc5da0a499a14e20b
isaacpedroza <isaacpedrozag@gmail.com>
2020-05-26 20:28:43 +1000

    Ansible updated with Swarm

--- Ansible/launch-nectar-servers.sh
+++ Ansible/launch-nectar-servers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-. ./unimelb-comp90024-2020-grp-01-openrc.sh; ansible-playbook --ask-become-pass launch_nectar.yaml -e 'ansible_python_interpreter=/usr/bin/python3'
+. ./unimelb-comp90024-2020-grp-01-openrc.sh; ansible-playbook --ask-become-pass launch_nectar.yaml -e 'ansible_python_interpreter=/usr/bin/python3' -i ./inventory/hosts.ini -vvv
